f201e495cad9aef04559b1cfc29ae6f2d840e8df
Sonu Rajput <103281970+SonuRajput1010@users.noreply.github.com>
2023-06-17 01:47:08 +0530

    Updated Solution - GfG to GitHub

--- Basic/Binary Search/binary-search.cpp
+++ Basic/Binary Search/binary-search.cpp
@@ -12,24 +12,22 @@ class Solution {
   public:
     int binarysearch(int arr[], int n, int k) {
         // code here
-int start =0;
-int end =n-1;
-int mid = start+(end-start)/2;
-while (start<=end){
-    if (arr[mid] == k){
-        return mid;
+        int s=0;
+        int e = n-1;
+        while(s<=e){
+            int mid = (s+e)/2;
+            if(arr[mid] == k){
+                return mid;
+            }
+            else if(arr[mid]>k){
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
+            }
+        }
+        return -1;
     }
-    if(arr[mid] < k ){
-        start = mid +1;
-    } else {
-        end = mid -1;
-    }
-    mid = start+(end-start)/2;
-}
-return -1;
-
-}
-    
 };
 
 //{ Driver Code Starts.
