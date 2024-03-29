22206ee95670a74dd5ce49a8e44a141facb84b5b
Sonu Rajput <sonusinghrajput9189@gmail.com>
2024-03-30 05:27:04 +0530

    Updated Solution - GfG to GitHub

--- Basic/Minimum element in BST/minimum-element-in-bst.cpp
+++ Basic/Minimum element in BST/minimum-element-in-bst.cpp
@@ -1,4 +1,6 @@
 //{ Driver Code Starts
+// Initial Template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -76,7 +78,45 @@ Node* buildTree(string str) {
     return root;
 }
 
-int minValue(Node* root);
+
+// } Driver Code Ends
+// User function Template for C++
+
+// Function to find the minimum element in the given BST.
+
+
+// struct Node {
+//     int data;
+//     struct Node* left;
+//     struct Node* right;
+// };
+// // Utility function to create a new Tree Node
+// Node* newNode(int val) {
+//     Node* temp = new Node;
+//     temp->data = val;
+//     temp->left = NULL;
+//     temp->right = NULL;
+
+//     return temp;
+// }
+
+class Solution {
+public:   
+    int minValue(Node* root) {
+        // Code here
+          if(root==NULL)
+        return -1;
+        
+        while(root->left!=NULL)
+        root=root->left;
+        
+        return root->data;
+    }
+};
+
+
+//{ Driver Code Starts.
+
 
 int main() {
 
@@ -86,41 +126,9 @@ int main() {
         string s;
         getline(cin, s);
         Node* root = buildTree(s);
-        cout << minValue(root) << endl;
+        Solution ob;
+        cout << ob.minValue(root) << endl;
     }
     return 1;
 }
 // } Driver Code Ends
-
-
-// Function to find the minimum element in the given BST.
-
-/*
-struct Node {
-    int data;
-    struct Node* left;
-    struct Node* right;
-};
-// Utility function to create a new Tree Node
-Node* newNode(int val) {
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-
-    return temp;
-}
-*/
-int minValue(Node* root) {
-    // Code here
-     if(root==NULL)
-    {
-        return -1; 
-    }
-    Node* temp = root;
-    
-    while(temp->left != NULL){
-        temp = temp->left;
-    }
-    return temp->data;
-}
