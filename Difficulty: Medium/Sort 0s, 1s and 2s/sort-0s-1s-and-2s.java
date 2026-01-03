9f2e52651673e7f725352cc8cbda955d48a283ce
Sonu Rajput <sonusinghrajput9189@gmail.com>
2026-01-04 03:14:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,29 +1,39 @@
 class Solution {
-    private void swap(int[] arr, int i, int j) {
+    public void swap(int[] arr, int i, int j){
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+        
     }
     public void sort012(int[] arr) {
         // code here
-        int low =0;
+        int low = 0;
         int mid = 0;
         int high = arr.length-1;
         
         while(mid <= high){
+                
             if(arr[mid] == 0){
                 swap(arr,low,mid);
                 low++;
                 mid++;
             }
+            
             else if(arr[mid] == 1){
                 mid++;
             }
+            
             else{
                 swap(arr,mid,high);
                 high--;
             }
+            
+            
         }
-        
     }
 }
+
+// 0    0    1      1h   2    2
+//            l,          m
+// m= 0
+// h= 5
